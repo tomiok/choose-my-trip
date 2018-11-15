42f7fdf180f4dbb4208907dcaeed1ad4ca2777e7
Tomas Lingotti <tomaslingotti@hotmail.com>
2018-11-14 23:39:21 -0300

    Add swagger config - create base structure for trip selector service

---
+++ trip-selector-service/src/main/java/com/tomiok/tripselectorservice/TripSelectorServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tomiok.tripselectorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripSelectorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripSelectorServiceApplication.class, args);
+	}
+}
