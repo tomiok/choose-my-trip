03b4f5725b6c08a87da9c7903cbfee83eaaaf59c
Tomas Lingotti <tomaslingotti@hotmail.com>
2018-11-16 13:20:22 -0300

    Update swagger configuration

--- trip-selector-service/src/main/java/com/tomiok/tripselectorservice/infra/SwaggerConfig.java
+++ trip-selector-service/src/main/java/com/tomiok/tripselectorservice/infra/SwaggerConfig.java
@@ -2,7 +2,10 @@ package com.tomiok.tripselectorservice.infra;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,6 +19,16 @@ public class SwaggerConfig {
     return new Docket(DocumentationType.SWAGGER_2)
         .select()
         .apis(RequestHandlerSelectors.basePackage("com.tomiok.tripselectorservice.web"))
+        .build().apiInfo(apiInfo());
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder().title("Trip finder/selector service")
+        .description("Trip finder/selector service REST API")
+        .contact(new Contact("Tomas Lingotti", "github.com/tomiok", "tomaslingotti@gmail.com"))
+        .license("Apache 2.0")
+        .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+        .version("1.0.0")
         .build();
   }
 }
