7747602abb5ae3586a73107916fffad6e6985cd2
Tomas Lingotti <tomaslingotti@hotmail.com>
2018-11-15 21:20:06 -0300

    Initial commit

---
+++ itinerary-discovery-server/src/main/java/com/tomiok/itinerarydiscoveryserver/ItineraryDiscoveryServerApplication.java
@@ -0,0 +1,12 @@
+package com.tomiok.itinerarydiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItineraryDiscoveryServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ItineraryDiscoveryServerApplication.class, args);
+  }
+}
